1ee283faf14b17fa05f5b21887d05ee7a2b3b20b
Pabi149 <Pabi09@icloud.com>
2021-10-24 19:15:08 -0500

    initial commit

--- Develop/planner.js
+++ Develop/planner.js
@@ -18,22 +18,4 @@ $(document).ready(function(){
     $("#17 .description").val(localStorage.getItem("17"));
     $("#18 .description").val(localStorage.getItem("18"));
     
-    function timeChange(){
-        var currentHour = moment().hour();
-
-        $('.time-block').each(function(){
-            var idTime = parseInt($(this).attr("id"));
-            if (idTime < currentHour){
-                $(this).addClass("past");
-            } else if (idTime === currentHour){
-                $(this).addClass("present");
-                $(this).removeClass("past");
-            } else {$(this).addClass("future");
-            $(this).removeClass("past");
-            $(this).removeClass("present");
-            }
-        })
-    }
-  timeChange();
-  var timeInterval = setInterval (timeChange, 2000);
-});
+  
